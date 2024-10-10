7cc45f19c97c475f1c3aa1a4e60e3d692075727b
benka <andrew.benkoczi@gmail.com>
2024-10-10 15:04:17 +0100

    add env var processing

--- pages/api/test.js
+++ pages/api/test.js
@@ -5,9 +5,10 @@ const cors = Cors({
 });
 */
 export default async function handler(req, res) {
+  const testEnvVar = process.env.TEST_ENV_VAR
   if (req.method !== 'POST') {
     res.status(403).send('Forbidden!');
     return;
   }
-  res.status(200).json({ message: 'Test message from the API' })
+  res.status(200).json({ message: `Test message from the API with ${testEnvVar}` })
 }
